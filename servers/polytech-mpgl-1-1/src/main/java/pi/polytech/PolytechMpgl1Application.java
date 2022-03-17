83492475a60c5681cad8c4d527840bb8efe244a1
chourabi-dev <chourabitahers62@gmail.com>
2022-03-17 20:27:51 +0100

    latest

---
+++ servers/polytech-mpgl-1-1/src/main/java/pi/polytech/PolytechMpgl1Application.java
@@ -0,0 +1,13 @@
+package pi.polytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolytechMpgl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolytechMpgl1Application.class, args);
+	}
+
+}
---
+++ servers/polytech-mpgl-1-1/src/test/java/pi/polytech/PolytechMpgl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.polytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolytechMpgl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
